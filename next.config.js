db6b73743c24792e3c20797b88406c640199dfa5
Hello-Misha <94431908+Hello-Misha@users.noreply.github.com>
2023-01-24 22:01:57 +0200

    fix the toogler | next.config - fs error

--- next.config.js
+++ next.config.js
@@ -3,4 +3,11 @@ const { i18n } = require("./next-i18next.config");
 
 module.exports = {
   i18n,
+  // reactStrictMode: false,
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+
+    return config;
+  },
 };
