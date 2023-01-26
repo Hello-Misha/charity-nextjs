4360785181fc38a046738babb6ab442dd90e241f
Hello-Misha <94431908+Hello-Misha@users.noreply.github.com>
2023-01-26 21:20:48 +0200

    Next.config changes

--- next.config.js
+++ next.config.js
@@ -4,9 +4,13 @@ const { i18n } = require("./next-i18next.config");
 module.exports = {
   i18n,
   // reactStrictMode: false,
-  webpack5: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false };
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+      config.resolve.fallback = {
+        fs: false,
+      };
+    }
 
     return config;
   },
